5c2dc1894a88f7908fe179d1acd3e84abd31140c
= <bbhpowpow@gmail.com>
2022-03-07 14:19:52 +0700

    Membuat API Endpoint Reset Password

---
+++ app/Http/Controllers/Api/Auth/PasswordController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class PasswordController extends Controller
+{
+    // use SendsPasswordResetEmails;
+
+    public function reset(Request $request)
+    {
+        $request->validate([
+            'password_old' => ['required', 'string', 'min:8'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        if (Hash::check($request->password_old, $request->user()->password)) {
+            $request->user()->update([
+                'password' => Hash::make($request->password)
+            ]);
+
+            return response()->json([
+                'message' => 'success',
+            ], Response::HTTP_OK);
+        }
+
+        return response()->json([
+            'message' => 'Update password failed.',
+        ], Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Auth\PasswordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,8 +26,8 @@ Route::prefix('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])
         ->middleware('auth:sanctum');
 
-    // Route::post('/password/reset', [PasswordController::class, 'reset'])
-    //     ->middleware('auth:sanctum');
+    Route::post('/password/reset', [PasswordController::class, 'reset'])
+        ->middleware('auth:sanctum');
     // Route::post('/password/forgot', [PasswordController::class, 'sendResetLinkEmail']);
 });
 
