1dbc0bfbea22f36caafc3db55a5f721d1cf5e2ab
= <bbhpowpow@gmail.com>
2022-03-07 21:18:52 +0700

    Membuat Halaman CRUD User

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Traits\ImageStorage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Yajra\DataTables\Facades\DataTables;
+
+class UserController extends Controller
+{
+    use ImageStorage;
+
+    /**
+     * Construct
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $data = User::query();
+
+            return DataTables::eloquent($data)
+                ->addColumn('action', function ($data) {
+                    return view('layouts._action', [
+                        'model' => $data,
+                        'edit_url' => route('user.edit', $data->id),
+                        'show_url' => route('user.show', $data->id),
+                        'delete_url' => route('user.destroy', $data->id),
+                    ]);
+                })
+                ->addIndexColumn()
+                ->rawColumns(['action'])
+                ->toJson();
+        }
+
+        // $users = User::paginate(5);
+        return view('pages.user.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('pages.user.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $photo = $request->file('image');
+
+        if ($photo) {
+            $request['photo'] = $this->uploadImage($photo, $request->name, 'profile');
+        }
+
+        $request['password'] = Hash::make($request->password);
+
+        User::create($request->all());
+
+        return redirect()->route('user.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return view('pages.user.show', compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('pages.user.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $photo = $request->file('image');
+
+        if ($photo) {
+            $request['photo'] = $this->uploadImage($photo, $request->name, 'profile', true, $user->photo);
+        }
+
+        if ($request->password) {
+            $request['password'] = Hash::make($request->password);
+        } else {
+            $request['password'] = $user->password;
+        }
+
+        $user->update($request->all());
+
+        return redirect()->route('user.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::find($id);
+
+        if ($user->photo) {
+            $this->deleteImage($user->photo, 'profile');
+        }
+
+        $user->delete();
+
+        return redirect()->route('user.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::resource('user', UserController::class);
